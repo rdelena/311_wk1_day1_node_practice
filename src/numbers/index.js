b4e8036e0c3ab3ebb7e28805b9718bc3356d2ec8
rdelena <rodnydelena@gmail.com>
2023-03-18 21:59:51 -0500

    added code for dates, numbers, & strings js

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,20 +1,27 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
-}
+  return num % 2 === 0;
+};
 
 const sum = (arr) => {
   // write code for numbers.sum
-
-}
+  return arr.reduce((a, b) => a + b);
+};
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
-}
+  for (let i = 0; i < arr.length; i++) {
+    for (let x = i + 1; x < arr.length; x++) {
+      if (arr[i] + arr[x] === sum) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 
 module.exports = {
   isEven,
   sum,
-  comboSum
-}
+  comboSum,
+};
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,20 +1,25 @@
 const split = (str, delim) => {
   // write code for strings.split
-
-}
+  return str.split(delim);
+};
 
 const pairs = (str) => {
   // write code for strings.pairs
-
-}
+  const pairArr = [];
+  for (let i = 0; i < str.length; i += 2) {
+    const pair = str.slice(i, i + 2);
+    pairArr.push(pair);
+  }
+  return pairArr;
+};
 
 const reverse = (str) => {
   // write code for strings.reverse
-
-}
+  return str.split("").reverse().join("");
+};
 
 module.exports = {
   split,
   pairs,
-  reverse
-}
+  reverse,
+};
